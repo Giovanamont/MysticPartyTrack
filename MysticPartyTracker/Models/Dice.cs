96f46ef023d899df655febc682361beaa0c450bc
Giovana <giovana280721@gmail.com>
2024-04-29 22:50:15 -0300

    MVVM

---
+++ MysticPartyTracker/Models/Dice.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MysticPartyTracker.Models
+{
+    public class Dice
+    {
+        public int Side { get; set; }
+        public int Quantity { get; set; }
+
+        public Dice(int side, int quantity)
+        {
+            Quantity = quantity;
+            Side = side;
+        }
+
+        public int Roll()
+        {
+            return new Random().Next(Side) + 1;
+        }
+
+        public List<int> RollDices()
+        {
+            List<int> dices = new List<int>([]);
+            for (int i = 0; i < Quantity; i++)
+            {
+                dices.Add(Roll());
+            }
+            return dices;
+        }
+    }
+}
